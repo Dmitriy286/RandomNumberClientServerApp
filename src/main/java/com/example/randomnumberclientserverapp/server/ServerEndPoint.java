7f8426fdd16c0dedb92ee253ba5dcb83fef90d5d
Dmitry286 <DShishlov@gmail.com>
2023-01-23 23:07:19 +0300

    fixed page reloading

--- src/main/java/com/example/randomnumberclientserverapp/server/ServerEndPoint.java
+++ src/main/java/com/example/randomnumberclientserverapp/server/ServerEndPoint.java
@@ -28,15 +28,17 @@ public class ServerEndPoint {
         clients = new CopyOnWriteArrayList<>();
         random = new Random();
         randomNumber = random.nextInt();
+        randomNumberGen = new ServerEndPoint.RandomNumberGen();
+        randomNumberGen.start();
     }
 
     @OnOpen
     public void onOpen(Session session, @PathParam("login") String login, EndpointConfig _) throws IOException {
         log.info("Login passed to ServerEndPoint: {}", login);
-        if (clients.isEmpty()) {
-            randomNumberGen = new ServerEndPoint.RandomNumberGen();
-            randomNumberGen.start();
-        }
+//        if (clients.isEmpty()) {
+//            randomNumberGen = new ServerEndPoint.RandomNumberGen();
+//            randomNumberGen.start();
+//        }
         this.session = session;
         this.session.getBasicRemote().sendText(String.valueOf(randomNumber));
         clients.add(this);
@@ -45,14 +47,16 @@ public class ServerEndPoint {
     }
 
     @OnClose
-    public void onClose(Session session, CloseReason reason) {
+    public void onClose(Session session, CloseReason reason) throws InterruptedException {
         log.warn("Socket closed: {}", reason.getReasonPhrase());
+//        log.warn("Check for reloading page or closing connection...");
         clients.remove(this);
+//        Thread.sleep(5000);
+//        if (clients.isEmpty()) {
+//            randomNumberGen.disable();
+//            randomNumberGen = null;
+//        }
         log.info("One of the clients has logged out");
-        if (clients.isEmpty()) {
-            randomNumberGen.disable();
-            randomNumberGen = null;
-        }
     }
 
     /**
